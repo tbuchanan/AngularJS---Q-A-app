c0bda4d83e6333cd3f17cbc83402508a3e491fc5
Tyler Buchanan <tmailbuchanan@yahoo.com>
2014-08-23 17:06:52 -0700

    removed some files

--- app/assets/javascripts/MyApp/mains/controllers/mains.js
+++ app/assets/javascripts/MyApp/mains/controllers/mains.js
@@ -1,8 +1,6 @@
 'use strict';
 
 function MainsCtrl($scope) {
-  var abc = 'hello there';
-  $scope.abc = abc;
 
 }
 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,4 @@ class HomeController < ApplicationController
   def index
   end
 
-  def create
-  end
-
-  def play
-  end
 end
